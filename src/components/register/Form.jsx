18d73aedc6eb2b6272812cc368d6a88d2005b244
DDev16 <dillondevera12@gmail.com>
2023-05-15 01:22:26 -0700

    register metamask

--- src/components/register/Form.jsx
+++ src/components/register/Form.jsx
@@ -1,35 +1,42 @@
 import Link from "next/link";
-import detectEthereumProvider from '@metamask/detect-provider';
-import Web3 from 'web3';
+import React, { useState } from 'react';
+import Web3 from "web3";
+import contractABI from "../common/user-credentials/registration.json"
 
-
-const handleMetamask = async () => {
-  const provider = await detectEthereumProvider();
-
-  if (provider) {
-    // From now on, this should always be true:
-    // provider === window.ethereum
-    startApp(provider); // initialize your app
-  } else {
-    console.log('Please install MetaMask!');
-  }
-};
-
-const startApp = async (ethereumProvider) => {
-  // Create web3 instance
-  const web3 = new Web3(ethereumProvider);
-
-  // Get list of accounts from Metamask
-  const accounts = await ethereumProvider.request({ method: 'eth_requestAccounts' });
-
-  if (accounts.length > 0) {
-    // User is connected with Metamask, you can save the account
-    // for registration, or interact with a smart contract here
-    console.log(accounts[0]); // Metamask Ethereum account
-  }
-};
+    
 
 const Form = () => {
+
+  const [log, setLog] = useState("");
+
+   
+    
+    
+
+    const handleRegisterWithMetamask = async () => {
+        setLog("Attempting to connect with MetaMask...");
+        if (typeof window.ethereum !== 'undefined') {
+            setLog("MetaMask is installed!");
+            const web3 = new Web3(window.ethereum);
+            try {
+                const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+                setLog(`Connected with the account ${accounts[0]}`);
+
+                const contractAddress = "0x04C89607413713Ec9775E14b954286519d836FEf"; 
+
+                const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+                await contract.methods.register().send({ from: accounts[0] });
+
+                setLog('User registered successfully');
+            } catch (error) {
+                setLog("User account already registered");
+            }
+        } else {
+            setLog('Non-Ethereum browser detected. You should consider trying MetaMask!');
+        }
+    }
+  
   return (
     <form action="#">
       <div className="heading text-center">
@@ -150,13 +157,14 @@ const Form = () => {
         {/* End .col */}
       </div>
       <div className="col-lg-6">
-  <button
+      <button
     type="button"
-    className="btn btn-block color-white bgc-gogle mb0 w-100"
-    onClick={handleMetamask}
-  >
-    <i className="fa fa-ethereum float-start mt5"></i> Metamask
-  </button>
+    className="btn btn-log w-100 btn-thm"
+    onClick={handleRegisterWithMetamask}
+>
+    Register with MetaMask
+</button>
+<pre>{log}</pre>
 </div>
 
       {/* more signin options */}
